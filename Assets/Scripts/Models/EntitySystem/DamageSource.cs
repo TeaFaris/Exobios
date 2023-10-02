4d37ac1ca14cedf741ddac9b57bed78a76262180
TeaFaris <teafaris@organizatsiya.site>
2023-10-03 00:30:40 +0500

    Damage Source death message replaced with death message format

--- Assets/Scripts/Models/EntitySystem/DamageSource.cs
+++ Assets/Scripts/Models/EntitySystem/DamageSource.cs
@@ -1,12 +1,26 @@
-﻿public sealed class DamageSource
+﻿using System;
+
+public sealed class DamageSource
 {
     public readonly Entity Source;
+    public readonly Entity Target;
 
-    public readonly string DeathMessage;
+    private readonly string deathMessageFormat;
 
-    public DamageSource(Entity source, string deathMessage = default)
+    public string FormattedDeathMessage => string.Format(deathMessageFormat, Source.ToString(), Target.ToString());
+
+    /// <param name="source">Entity that damaged.</param>
+    /// <param name="target">Entity who was damaged.</param>
+    /// <param name="deathMessageFormat">
+    /// Supports formatting,
+    /// where {0} is <see cref="Target"/> => <see cref="Entity.ToString"/> (damaged entity)
+    /// and {1} <see cref="Source"/> => <see cref="Entity.ToString"/> (entity that damaged it).
+    /// </param>
+    public DamageSource(Entity source, Entity target, string deathMessageFormat = "{0} was killed by {1}.")
     {
         Source = source;
-        DeathMessage = deathMessage;
+        Target = target;
+
+        this.deathMessageFormat = deathMessageFormat;
     }
 }
