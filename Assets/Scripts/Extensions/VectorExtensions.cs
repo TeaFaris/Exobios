5e07758cc9368f0faf3fd56ad167611ede4fe5d2
TeaFaris <teafaris@organizatsiya.site>
2023-10-03 16:31:15 +0500

    Physics additional methods were added

---
+++ Assets/Scripts/Extensions/VectorExtensions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public static class VectorExtensions
+{
+    public static Vector3 VectorMa(Vector3 start, float scale, Vector3 direction)
+    {
+        return new Vector3()
+        {
+            x = start.x + (direction.x * scale),
+            y = start.y + (direction.y * scale),
+            z = start.z + (direction.z * scale)
+        };
+    }
+    
+}
